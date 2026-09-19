1ba0650f1305b27ff98f28b3d2022fc2cf070d6a
aditraj2410-crypto <adit.raj2410@gmail.com>
2026-09-19 10:17:56 +0530

    Delete demoo directory

--- demoo/src/backend-sync.js
+++
@@ -1,9 +0,0 @@
-const socket = io();
-
-socket.on('queue_snapshot', (patients) => {
-  window.dispatchEvent(new CustomEvent('queue_snapshot', { detail: patients }));
-});
-
-socket.on('new_patient_arrived', (patient) => {
-  window.dispatchEvent(new CustomEvent('new_patient_arrived', { detail: patient }));
-});
